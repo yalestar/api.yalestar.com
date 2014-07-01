59534710d1c8f3f368880f4c29ca59287ae325d8
yalestar <yalestar@gmail.com>
2014-06-30 20:58:52 -0700

    Loggery

--- config/db.js
+++ config/db.js
@@ -8,7 +8,10 @@ var config = {
   "pw": "",
   "port": 27017
 };
-
+console.log("===========================================================");
+console.log("process.env.MONGOHQ_URL coming up:");
+console.log(process.env.MONGOHQ_URL);
+console.log("===========================================================");
 var port = (config.port.length > 0) ? ":" + config.port : '';
 var login = (config.user.length > 0) ? config.user + ":" + config.pw + "@" : '';
 var uriString = process.env.MONGOHQ_URL ||  "mongodb://" + login + config.host + port + "/" + config.db;
