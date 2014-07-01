67e5e5ba5b8e6f631cdd4a9f83d39bdb72a2db53
yalestar <yalestar@gmail.com>
2014-06-30 21:00:34 -0700

    Loggery

--- config/db.js
+++ config/db.js
@@ -8,14 +8,19 @@ var config = {
   "pw": "",
   "port": 27017
 };
-console.log("===========================================================");
-console.log("process.env.MONGOHQ_URL coming up:");
-console.log(process.env.MONGOHQ_URL);
-console.log("===========================================================");
+// console.log("===========================================================");
+// console.log("process.env.MONGOHQ_URL coming up:");
+// console.log(process.env.MONGOHQ_URL);
+// console.log("===========================================================");
 var port = (config.port.length > 0) ? ":" + config.port : '';
 var login = (config.user.length > 0) ? config.user + ":" + config.pw + "@" : '';
 var uriString = process.env.MONGOHQ_URL ||  "mongodb://" + login + config.host + port + "/" + config.db;
-var uriString = "mongodb://" + config.host + port + "/" + config.db;
+// var uriString = "mongodb://" + config.host + port + "/" + config.db;
+
+console.log("===========================================================");
+console.log("uriString:");
+console.log(uriString);
+console.log("===========================================================");
 
 var mongoOptions = { db: { safe: true } };
 
