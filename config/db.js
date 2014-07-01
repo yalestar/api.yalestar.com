a1c5e87ee846bb762fbb43155b31b70a3149abbf
yalestar <yalestar@gmail.com>
2014-06-30 20:51:49 -0700

    Use env var

--- config/db.js
+++ config/db.js
@@ -11,7 +11,7 @@ var config = {
 
 var port = (config.port.length > 0) ? ":" + config.port : '';
 var login = (config.user.length > 0) ? config.user + ":" + config.pw + "@" : '';
-//var uriString =  process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||  "mongodb://" + login + config.host + port + "/" + config.db;
+var uriString = process.env.MONGOHQ_URL ||  "mongodb://" + login + config.host + port + "/" + config.db;
 var uriString = "mongodb://" + config.host + port + "/" + config.db;
 
 var mongoOptions = { db: { safe: true } };
